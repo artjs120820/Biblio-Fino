0bef0329394ac08c2c11c7cde597e66b2e79c887
artjs120820 <arturosilvera1101@gmail.com>
2025-03-03 15:56:56 -0500

    reserva lgn

--- app/(client)/reservas/page.jsx
+++ app/(client)/reservas/page.jsx
@@ -1,5 +1,22 @@
+'use client'
+import { useState } from "react";
+import LoginModal from "../../(empty)/login/page";
+
 export default function Page() {
+    const [mostrarLogin, setMostrarLogin] = useState(false);
+
     return (
-        <h1>Reservas</h1>
+        <div className="flex flex-col items-start justify-center p-8">
+            <h1 className="text-3xl font-bold mb-4">Reservas</h1>
+            <p className="text-lg mb-4">Para reservar libros, debes iniciar sesión primero.</p>
+            <button 
+                onClick={() => setMostrarLogin(true)} 
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
+            >
+                Iniciar sesión
+            </button>
+
+            {mostrarLogin && <LoginModal showLogin={mostrarLogin} setShowLogin={setMostrarLogin}  />}
+        </div>
     );
 }
